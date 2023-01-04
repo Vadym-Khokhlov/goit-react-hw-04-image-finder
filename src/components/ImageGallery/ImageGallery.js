1a5dacf867886559bcc36e8719504961f62e14e0
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-01-04 22:47:34 +0200

    imageGallery hooks refactoring

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../styles.css';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
-export default class ImageGallery extends Component {
-  static propTypes = {
-    images: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        webformatURL: PropTypes.string.isRequired,
-        largeImageURL: PropTypes.string.isRequired,
-      })
-    ).isRequired,
-  };
+export default function ImageGallery({ images }) {
+  // static propTypes = {
+  //   images: PropTypes.arrayOf(
+  //     PropTypes.shape({
+  //       id: PropTypes.number.isRequired,
+  //       webformatURL: PropTypes.string.isRequired,
+  //       largeImageURL: PropTypes.string.isRequired,
+  //     })
+  //   ).isRequired,
+  // };
 
-  render() {
-    return (
-      <ul className="ImageGallery">
-        {this.props.images.map(image => (
-          <ImageGalleryItem
-            key={image.id}
-            webformatURL={image.webformatURL}
-            largeImageURL={image.largeImageURL}
-          />
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul className="ImageGallery">
+      {images.map(image => (
+        <ImageGalleryItem
+          key={image.id}
+          webformatURL={image.webformatURL}
+          largeImageURL={image.largeImageURL}
+        />
+      ))}
+    </ul>
+  );
 }
