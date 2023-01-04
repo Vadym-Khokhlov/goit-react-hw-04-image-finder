4264c50f09eea16cbc92c29e3fce70a719b5e535
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2023-01-04 22:41:39 +0200

    imageGalleryItem hooks refactoring

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Modal from '../Modal';
 import '../styles.css';
 
-export default class ImageGalleryItem extends Component {
-  state = {
-    showModal: false,
+export default function ImageGalleryItem({ id, webformatURL, largeImageURL }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(showModal => !showModal);
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
+  return (
+    <li key={id} className="ImageGalleryItem">
+      <img
+        src={webformatURL}
+        alt=""
+        className="ImageGalleryItem-image"
+        onClick={toggleModal}
+      />
 
-  render() {
-    return (
-      <li key={this.props.id} className="ImageGalleryItem">
-        <img
-          src={this.props.webformatURL}
-          alt=""
-          className="ImageGalleryItem-image"
-          onClick={this.toggleModal}
-        />
-
-        {this.state.showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={this.props.largeImageURL} alt="" />
-          </Modal>
-        )}
-      </li>
-    );
-  }
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={largeImageURL} alt="" />
+        </Modal>
+      )}
+    </li>
+  );
 }
